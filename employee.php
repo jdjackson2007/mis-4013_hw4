cb5f9a4d2052c5a1e6ca4946115506f7cfea4114
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-29 14:25:09 -0600

    Create employee.php

---
+++ employee.php
@@ -0,0 +1,9 @@
+<?php
+Require_Once("util-db.php");
+Require_Once("model-employee.php");
+$pageTitle = "EMPLOYEE";
+include "view-header.php";
+$employees = selectEmployee();
+include "view-employee.php";
+include "view-footer.php";
+?>
