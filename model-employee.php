569c00f5d4bed12efcbcfb7332a09486da482c34
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-29 16:46:16 -0600

    Update model-employee.php

--- model-employee.php
+++ model-employee.php
@@ -12,4 +12,61 @@ function selectEmployee() {
         throw $e;
     }
 }
+function insertEmployee($firstName, $lastName, $email, $phone, $hireDate, $jobTitle) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO employee_table (Employee_FirstName, Employee_LastName, Employee_Email, Employee_Phone, Employee_HireDate, Employee_JobTitle)
+        VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $firstName, $lastName, $email, $phone, $hireDate, $jobTitle);
+        $stmt->execute();
+        $result = $stmt->affected_rows; 
+        $stmt->close();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        if ($conn) {
+            $conn->close();
+        }
+        throw $e; 
+    }
+}
+function updateEmployee($employeeId, $firstName, $lastName, $email, $phone, $hireDate, $jobTitle) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE employee_table 
+                                SET Employee_FirstName = ?, Employee_LastName = ?, Employee_Email = ?, Employee_Phone = ?, Employee_HireDate = ?, Employee_JobTitle = ?
+                                WHERE Employee_ID = ?");
+        $stmt->bind_param("ssssssi", $firstName, $lastName, $email, $phone, $hireDate, $jobTitle, $employeeId);
+        $stmt->execute();
+        $result = $stmt->affected_rows; 
+        $stmt->close();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        if ($conn) {
+            $conn->close();
+        }
+        throw $e;
+    }
+}
+function deleteEmployee($employeeId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM employee_table WHERE Employee_ID = ?");
+        $stmt->bind_param("i", $employeeId); 
+        $stmt->execute();
+        $result = $stmt->affected_rows; 
+        $stmt->close();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        if ($conn) {
+            $conn->close();
+        }
+        throw $e;
+    }
+}
+
+
+
 ?>
