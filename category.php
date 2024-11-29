0164c9f784828b3403f152848c9ad88e296df2bc
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-29 14:20:36 -0600

    Create category.php

---
+++ category.php
@@ -0,0 +1,9 @@
+<?php
+Require_Once("util-db.php");
+Require_Once("model-category.php");
+$pageTitle = "CATEGORY";
+include "view-header.php";
+$categories = selectCategory();
+include "view-category.php";
+include "view-footer.php";
+?>
