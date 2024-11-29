8534b39a0af5308037a102b902f7de1047555670
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-29 14:06:12 -0600

    Delete category.php

--- category.php
+++
@@ -1,9 +0,0 @@
-<?php
-Require_Once("util-db.php");
-  Require_Once("model-category.php");
-$pageTitle = "CATEGORY";
-include "view-header.php";
-$categories = selectCategory();
-include "view-category.php";
-include "view-footer.php";
-?>
