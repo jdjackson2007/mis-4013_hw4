2a2ccff41fc2724ab13490df2bed31a3885da693
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 17:06:07 -0600

    Update employee-job-title-list.php

--- employee-job-title-list.php
+++ employee-job-title-list.php
@@ -1,1 +1,10 @@
-
+<select class="form-select" id="EmployeeJobTitle" name="Employee_JobTitle">
+    <?php 
+        while ($jobTitle = $jobTitleList->fetch_assoc()) { ?>
+            <option value="<?php echo $jobTitle['Employee_JobTitle']; ?>">
+                <?php echo $jobTitle['Employee_JobTitle']; ?>
+            </option>
+    <?php 
+        } 
+    ?>
+</select>
