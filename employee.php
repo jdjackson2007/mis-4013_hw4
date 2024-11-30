db59fefab4bb96c7c2c61de1b082fc13c8927093
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 09:26:43 -0600

    Update employee.php

--- employee.php
+++ employee.php
@@ -3,6 +3,12 @@ Require_Once("util-db.php");
 Require_Once("model-employee.php");
 $pageTitle = "EMPLOYEE";
 include "view-header.php";
+
+if (isset([$_POST 'actionType']){
+    switch ($_Post 'actionType') {
+    case "ADD" 
+    }
+}
 $employees = selectEmployee();
 include "view-employee.php";
 include "view-footer.php";
