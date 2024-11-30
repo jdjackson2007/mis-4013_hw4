44eb1e4ce5552bad2f2b71d2a3caafc785d7ed92
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 10:26:14 -0600

    Update view-employee.php

--- view-employee.php
+++ view-employee.php
@@ -31,9 +31,16 @@
           <td><?php echo $employee['Employee_Phone']; ?></td>
           <td><?php echo $employee['Employee_HireDate']; ?></td>
           <td><?php echo $employee['Employee_JobTitle']; ?></td>
-          <td> 
-            <a href="edit-employee.php?id=<?php echo $employee['Employee_ID']; ?>" class="btn btn-warning">Edit</a>
-          </td>
+         <td>
+  <a href="edit-employee.php?id=<?php echo $employee['Employee_ID']; ?>" class="btn btn-warning">Edit</a>
+  <a href="delete-employee.php?id=<?php echo $employee['Employee_ID']; ?>" 
+     class="btn btn-danger" 
+     onclick="return confirm('Are you sure you want to delete this employee?');">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+    </svg>
+  </a>
+</td>
         </tr>
       <?php } ?>
     </tbody>
