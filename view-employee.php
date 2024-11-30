fbaa2ab6d8f75d0897653f74616cdb2fa5e3a004
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 14:16:04 -0600

    Update view-employee.php

--- view-employee.php
+++ view-employee.php
@@ -19,6 +19,9 @@
       <th>Employee Phone</th>
       <th>Employee Hire Date</th>
         <th>Employee Job Title</th>
+        <th></th>
+        <th></th>
+        <th></th>
       </tr>
      
  
@@ -34,6 +37,22 @@
    <td><?php echo $employee ['Employee_Phone'];?></td>
    <td><?php echo $employee ['Employee_HireDate'];?></td>
    <td><?php echo $employee ['Employee_JobTitle'];?></td>
+    <td>
+      
+    </td>
+   <td>
+    <form method="POST" action="employee.php" style="display:inline;">
+        <input type="hidden" name="action" value="delete">
+        <input type="hidden" name="employee_id" value="<?php echo $employee['employee_id']; ?>">
+        <button type="submit" class="btn btn-danger btn-sm" title="Delete Employee">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
+        </button>
+    </form>
+</td>
+
   
     
   </tr>
