e68cddf410437dc3c23f7f4f97f2369c08d032a6
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 12:14:22 -0600

    Update employee.php

--- employee.php
+++ employee.php
@@ -4,22 +4,8 @@ Require_Once("model-employee.php");
 $pageTitle = "EMPLOYEE";
 include "view-header.php";
 
-if (isset([$_POST 'actionType']){
-    switch ($_Post 'actionType') {
-    case "Add" :
-        insertEmployee($_Post['$firstName'],$_Post ['$lastName'], $_Post['$email'], $_Post['$phone'], $_Post['$hireDate'], $_Post['$jobTitle']{
-                       echo '<div class="alert alert-light" role="alert">
-                            New Employee Successfully added.
-                            </div>';{
-                       else '<div class="alert alert-light" role="alert">
-                            Error Adding New Employee.
-                            </div>';
-                            }
-                                    }
-                       Break;
-    }
-}
+
 $employees = selectEmployee();
-include "view-employee.php";
+
 include "view-footer.php";
 ?>
