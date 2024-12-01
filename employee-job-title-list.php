432aa41eec6098bb1432f3a02d706619e4d8eb16
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 19:02:15 -0600

    Update employee-job-title-list.php

--- employee-job-title-list.php
+++ employee-job-title-list.php
@@ -1,10 +1,9 @@
-<select class="form-select" id="EmployeeJobTitle" name="Employee_JobTitle">
-    <?php 
-        while ($jobTitle = $jobTitleList->fetch_assoc()) { ?>
-            <option value="<?php echo $jobTitle['Employee_JobTitle']; ?>">
-                <?php echo $jobTitle['Employee_JobTitle']; ?>
-            </option>
-    <?php 
-        } 
-    ?>
+<select class="form-select" id="EmployeeJobTitle<?php echo $employee['employee_id']; ?>" name="Employee_JobTitle" required>
+  <option value="" disabled>Select Job Title</option>
+  <?php while ($jobTitle = $JobTitleList->fetch_assoc()) { ?>
+    <option value="<?php echo htmlspecialchars($jobTitle['Employee_JobTitle']); ?>" 
+      <?php echo ($employee['Employee_JobTitle'] === $jobTitle['Employee_JobTitle']) ? 'selected' : ''; ?>>
+      <?php echo htmlspecialchars($jobTitle['Employee_JobTitle']); ?>
+    </option>
+  <?php } ?>
 </select>
