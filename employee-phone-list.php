d593ba647143bd8d6ab111840ac726dcc7a7e089
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 19:01:44 -0600

    Update employee-phone-list.php

--- employee-phone-list.php
+++ employee-phone-list.php
@@ -1,10 +1,9 @@
-<select class="form-select" id="EmployeePhone" name="Employee_Phone">
-    <?php 
-          while ($phone = $phoneList->fetch_assoc()) { ?>
-              <option value="<?php echo $phone['Employee_Phone']; ?>">
-            <?php echo $phone['Employee_Phone']; ?>
-        </option>
-    <?php
-      }
-    ?>
+<select class="form-select" id="EmployeePhone<?php echo $employee['employee_id']; ?>" name="Employee_Phone" required>
+  <option value="" disabled>Select Phone Number</option>
+  <?php while ($phone = $PhoneList->fetch_assoc()) { ?>
+    <option value="<?php echo htmlspecialchars($phone['Employee_Phone']); ?>" 
+      <?php echo ($employee['Employee_Phone'] === $phone['Employee_Phone']) ? 'selected' : ''; ?>>
+      <?php echo htmlspecialchars($phone['Employee_Phone']); ?>
+    </option>
+  <?php } ?>
 </select>
